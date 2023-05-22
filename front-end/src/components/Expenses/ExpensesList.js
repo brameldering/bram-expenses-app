f3819bcf962e1bcc89ce3abb75690d39daaf8bdc
brameldering <bram.eldering@gmail.com>
2023-05-22 08:16:59 +0200

    ExpensesList: added sort on dates

--- front-end/src/components/Expenses/ExpensesList.js
+++ front-end/src/components/Expenses/ExpensesList.js
@@ -3,13 +3,19 @@ import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
   // check if there are expenses and if not then show default message
-  if (props.items.length === 0) {
+  const expenses = props.items;
+  if (expenses.length === 0) {
     return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
   }
+  // sort expenses by date ascending
+  expenses.sort((a, b) => {
+    return new Date(a.date) - new Date(b.date);
+  });
+
   // if there are expenses then show them
   return (
     <ul className='expenses-list'>
-      {props.items.map((item) => (
+      {expenses.map((item) => (
         <li key={item.id}>
           <ExpenseItem key={item.id} item={item} />
         </li>
