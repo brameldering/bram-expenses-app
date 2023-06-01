c31b15b5bd9f0bfbb81d922699c6440476b9bb52
brameldering <bram.eldering@gmail.com>
2023-06-01 16:23:10 +0200

    Replaced AuthContext.Consumer with useContext hook

--- front-end/src/components/MainHeader/Navigation.js
+++ front-end/src/components/MainHeader/Navigation.js
@@ -1,36 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Button from "../UI/Button/Button";
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+  const context = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <Button onClick={props.onLogout}>Logout</Button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <Button onClick={props.onLogout}>Logout</Button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
